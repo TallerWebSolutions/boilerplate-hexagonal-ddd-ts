7587b6e3cec5c3611b89af6cd26a60bd8f08bc17
AlicinhaDavid <alicedaviddossantos@gmail.com>
2022-02-22 19:32:43 -0300

    refactor: change products mock to match the right response DTO

--- src/modules/mock.ts
+++ src/modules/mock.ts
@@ -3,15 +3,21 @@ export const productsMock = [
     id: "1",
     description: "chicken pie",
     stockAmount: 10,
+    salePrice: 20,
+    costPrice: 10,
   },
   {
     id: "2",
     description: "cheese pie",
     stockAmount: 15,
+    salePrice: 20,
+    costPrice: 10,
   },
   {
     id: "3",
     description: "ham pie",
     stockAmount: 0,
+    salePrice: 20,
+    costPrice: 10,
   },
 ];
