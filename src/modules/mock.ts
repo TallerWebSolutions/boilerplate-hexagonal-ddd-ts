cf09bd768cebcb001eecc449d9f01c32e2197f01
AlicinhaDavid <alicedaviddossantos@gmail.com>
2022-02-22 10:50:52 -0300

    feat: create products mock to simulate a database data

---
+++ src/modules/mock.ts
@@ -0,0 +1,17 @@
+export const productsMock = [
+  {
+    id: "1",
+    description: "chicken pie",
+    stockAmount: 10,
+  },
+  {
+    id: "2",
+    description: "cheese pie",
+    stockAmount: 15,
+  },
+  {
+    id: "3",
+    description: "ham pie",
+    stockAmount: 0,
+  },
+];
